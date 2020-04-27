7b902a8e6ce0deff69d9d80d530e659a6dfe7710
Lucas Albuquerque <lucas.5a@stelo.com.br>
2020-04-27 11:12:48 -0300

    add navbar component using styled components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import history from './Services/history'
 import { BrowserRouter, Route } from 'react-router-dom';
 import Checkout from './Pages/Checkout'
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
     <BrowserRouter history={history}>
+      <Navbar />
        <Route path="/" component={Checkout} exact={true}></Route>
     </BrowserRouter>
   );
