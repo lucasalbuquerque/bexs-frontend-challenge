c11ca0b0ad4059efac47663cc916d6b5490fc10a
Lucas Albuquerque <lucas.5a@stelo.com.br>
2020-04-27 11:21:59 -0300

    add global style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import history from './Services/history'
 import { BrowserRouter, Route } from 'react-router-dom';
-import Checkout from './Pages/Checkout'
-import Navbar from './Components/Navbar'
+import Checkout from './Pages/Checkout';
+import Navbar from './Components/Navbar';
+import GlobalStyle from './styles';
 
 function App() {
   return (
     <BrowserRouter history={history}>
+      <GlobalStyle />
       <Navbar />
        <Route path="/" component={Checkout} exact={true}></Route>
     </BrowserRouter>
