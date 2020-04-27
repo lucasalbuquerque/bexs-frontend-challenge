a7509056f65f9ab50d270bf0a24103db95794809
Lucas Albuquerque <lucas.5a@stelo.com.br>
2020-04-27 11:58:44 -0300

    add style to checkout component

---
+++ src/Pages/Checkout/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Main = styled.div `
+  margin-top: 65px;
+`;
+
+export const Content = styled.div `
+  background: #ffffff;
+`;
+
+export const Left = styled.div `
+  width: 352px;
+  height: 596px;
+  background: #DE4B4B;
+`;
