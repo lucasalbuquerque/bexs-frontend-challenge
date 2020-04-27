451e75554ec2255455d2902e9770d6a3defecc9f
Lucas Albuquerque <lucas.5a@stelo.com.br>
2020-04-27 10:53:59 -0300

    remove unused hash router import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import history from './services/history'
-import { HashRouter, BrowserRouter, Route } from 'react-router-dom';
+import history from './Services/history'
+import { BrowserRouter, Route } from 'react-router-dom';
 import Checkout from './Pages/Checkout'
 
 function App() {
