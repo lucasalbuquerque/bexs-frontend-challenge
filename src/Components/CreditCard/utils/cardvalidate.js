a8c4f026408fdc0f4701c5f8cfae11d368d84adc
Lucas Albuquerque <lucas.5a@stelo.com.br>
2020-04-29 15:16:27 -0300

    add credit types and card validation regex module

---
+++ src/Components/CreditCard/utils/cardvalidate.js
@@ -0,0 +1,23 @@
+import * as flag from './types';
+const visaRegex = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+const mastercardRegex = /^(?:5[1-5][0-9]{14})$/;
+const amexRegex = /^(?:3[47][0-9]{13})$/;
+const discoverRegex = /^(?:6(?:011|5[0-9][0-9])[0-9]{12})$/;
+
+const foundCreditCardFlag = (value) => {
+  if(!value)
+  return
+  const formattedValue = value.replace(/[^0-9]/g, '');
+  if (visaRegex.test(formattedValue)) {
+    return flag.visa;
+  } else if(mastercardRegex.test(formattedValue)) {
+    return flag.master;
+  } else if(amexRegex.test(formattedValue)) {
+    return flag.amex;
+  } else if(discoverRegex.test(formattedValue)) {
+    return flag.discover;
+  }
+  return flag.def;
+}
+
+export default foundCreditCardFlag
---
+++ src/Components/CreditCard/utils/types.js
@@ -0,0 +1,5 @@
+export const visa = "VISA";
+export const master = "MASTER";
+export const amex = "AMEX";
+export const discover = "DISCOVER";
+export const def = "DEFAULT";
