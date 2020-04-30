616429b7d9e38a8f7a32f61d47951fa400e89d96
Lucas Albuquerque <lucas.5a@stelo.com.br>
2020-04-30 12:10:41 -0300

    add build folder

---
+++ build/precache-manifest.697c94b3e337488410bbee5142be86b4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc6bfdd8e0e6bf13ef3e5dcaab1affb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a07ac030f0d237145ef",
+    "url": "/static/js/2.bb02659d.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.bb02659d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f4a00a08e84583a97d8",
+    "url": "/static/js/main.0d689ccc.chunk.js"
+  },
+  {
+    "revision": "d1a60bc649c3c85be03a",
+    "url": "/static/js/runtime-main.0e016936.js"
+  },
+  {
+    "url": "/static/media/1c45e28f8e86cc89876f003b953cc3e9.1c45e28f.ttf"
+  },
+  {
+    "url": "/static/media/1c45e28f8e86cc89876f003b953cc3e9.20602ce5.woff2"
+  },
+  {
+    "url": "/static/media/1c45e28f8e86cc89876f003b953cc3e9.879e904a.woff"
+  },
+  {
+    "url": "/static/media/1c45e28f8e86cc89876f003b953cc3e9.a48005b5.eot"
+  },
+  {
+    "revision": "e113450193fb9989c6a7a0676d3789dd",
+    "url": "/static/media/amex.e1134501.png"
+  },
+  {
+    "revision": "8a9c3f27120c9bf4e7ef03c8998ff11a",
+    "url": "/static/media/discover.8a9c3f27.png"
+  },
+  {
+    "revision": "fb5b50f9d0f9fdf56be5f887c5bd0d37",
+    "url": "/static/media/logo.fb5b50f9.png"
+  },
+  {
+    "revision": "18bf4ee0dd2e2d4ddc937cdd10a62b6b",
+    "url": "/static/media/master.18bf4ee0.png"
+  }
+]);
